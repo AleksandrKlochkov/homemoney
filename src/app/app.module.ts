a204fed038120d91015862c2f308de29fbfa6045
Alex <surfer.ru@mail.ru>
2019-09-22 02:59:23 +0400

    add page login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { AuthModule } from './auth/auth.module'
+import { AppRoutingModule } from './app-routing.module'
+import { AuthRoutingModule } from './auth/auth-routing.module'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthModule,
+    AppRoutingModule,
+    AuthRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
